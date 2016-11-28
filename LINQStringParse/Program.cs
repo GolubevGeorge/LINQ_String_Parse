b8f1e9023e81619428a9adcf61d92caa6fb381c5
unknown <golubev_georgiy@ukr.net>
2016-11-28 13:46:31 +0200

    1st commit

---
+++ LINQStringParse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQStringParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 55;
+
+            string phrase = "The “C# Professional” course includes the topics I discuss in my CLR via C# book and" +
+            "teaches how the CLR works thereby showing you how to develop applications and reusable" +
+            "components for the .NET Framework.";
+
+            string[] splitPhrase = phrase.Split(new char[] { ' ' });
+
+            List<string> listOfSplitPhrase = splitPhrase.ToList<string>();
+            IEnumerable<string> uniqListOfSplitPhrase = listOfSplitPhrase.Distinct();
+
+            var orderedWords =
+                from word in uniqListOfSplitPhrase
+                orderby word.Length
+                group word by word.Length;
+
+            foreach (var orderedWord in orderedWords)
+            {
+                int count = orderedWord.Count(p => p.Length == orderedWord.Key);
+                Console.WriteLine("\nWords of length: {0}, Count: {1}", orderedWord.Key, count);
+
+                foreach (var word in orderedWord)
+                {
+                    Console.WriteLine(word);
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
